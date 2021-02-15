efa594bb27c708f56f45cc421bfcb64a9c80091c
a_semin <nemiroff666_88@mail.ru>
2021-02-15 21:44:21 +0300

    "Выполнил задание цикл for: Оценки "

--- 3_for.py
+++ 3_for.py
@@ -15,7 +15,31 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    school_students = [{'school_class': '1a', 'score': [3, 4, 4, 5, 2]},
+                       {'school_class': '2a', 'score': [3, 4, 4, 5, 3]},
+                       {'school_class': '3a', 'score': [3, 4, 4, 5, 5]}
+    ]
+
+    scores_sum = 0
+    for score in school_students[0]['score']:
+        scores_sum += score
+    scores_avg1 = scores_sum / len(school_students[0]['score'])
+    print(f"Средняя оценка класса '1а' {scores_avg1}")
+
+    for score in school_students[1]['score']:
+        scores_sum += score
+    scores_avg2 = scores_sum / len(school_students[1]['score'])
+    print(f"Средняя оценка класса '2а' {scores_avg2}")
+
+    for score in school_students[2]['score']:
+        scores_sum += score
+    scores_avg3 = scores_sum / len(school_students[2]['score'])
+    print(f"Средняя оценка класса '3а' {scores_avg3}")
+
+    score_avg = 3
+    scores_sum = scores_avg1 + scores_avg2 + scores_avg3
+    scores_avg = scores_sum / score_avg
+    print(f"Средняя оценка школы {scores_avg}")
+
 if __name__ == "__main__":
     main()
