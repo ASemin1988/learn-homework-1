85b946b608abb60932257be960880a909c295abe
a_semin <nemiroff666_88@mail.ru>
2021-02-19 13:14:56 +0300

    "Исправил замечание 3_for

--- 3_for.py
+++ 3_for.py
@@ -10,6 +10,7 @@
 * Посчитать и вывести средний балл по каждому классу.
 """
 
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
@@ -17,29 +18,19 @@ def main():
     """
     school_students = [{'school_class': '1a', 'score': [3, 4, 4, 5, 2]},
                        {'school_class': '2a', 'score': [3, 4, 4, 5, 3]},
-                       {'school_class': '3a', 'score': [3, 4, 4, 5, 5]}
+                       {'school_class': '3a', 'score': [3, 4, 4, 5, 5]},
+                       {'school_class': '4a', 'score': [3, 4, 4, 5, 5]},
     ]
 
-    scores_sum = 0
-    for score in school_students[0]['score']:
-        scores_sum += score
-    scores_avg1 = scores_sum / len(school_students[0]['score'])
-    print(f"Средняя оценка класса '1а' {scores_avg1}")
-
-    for score in school_students[1]['score']:
-        scores_sum += score
-    scores_avg2 = scores_sum / len(school_students[1]['score'])
-    print(f"Средняя оценка класса '2а' {scores_avg2}")
-
-    for score in school_students[2]['score']:
-        scores_sum += score
-    scores_avg3 = scores_sum / len(school_students[2]['score'])
-    print(f"Средняя оценка класса '3а' {scores_avg3}")
-
-    score_avg = 3
-    scores_sum = scores_avg1 + scores_avg2 + scores_avg3
-    scores_avg = scores_sum / score_avg
-    print(f"Средняя оценка школы {scores_avg}")
+    test = list()
+    for klass_sum in school_students:
+        klass = sum(klass_sum['score'])
+        scores_avg1 = klass / len(klass_sum['score'])
+        test.append(scores_avg1)
+        print(f"Средняя оценка класса {scores_avg1}")
+    test_sum = sum(test)
+    sum_school = test_sum / len(test)
+    print(sum_school)
 
 if __name__ == "__main__":
     main()
